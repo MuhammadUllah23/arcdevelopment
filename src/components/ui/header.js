453a06de0b349d23086369f28df07d642985f471
Muhammad <muhammadhullah23@gmail.com>
2022-01-28 16:18:50 -0500

    Copy ElevateScroll function from Material UI
    It is to elevate App Bar on scroll to communicate that the user is not at the top of the page

--- src/components/ui/header.js
+++ src/components/ui/header.js
@@ -2,6 +2,31 @@ import React from "react";
 import { AppBar } from "@material-ui/core/";
 import { Toolbar } from "@material-ui/core/";
 
+function ElevationScroll(props) {
+    const { children, window } = props;
+    // Note that you normally won't need to set the window ref as useScrollTrigger
+    // will default to window.
+    // This is only being set here because the demo is in an iframe.
+    const trigger = useScrollTrigger({
+      disableHysteresis: true,
+      threshold: 0,
+      target: window ? window() : undefined,
+    });
+  
+    return React.cloneElement(children, {
+      elevation: trigger ? 4 : 0,
+    });
+  }
+  
+  ElevationScroll.propTypes = {
+    children: PropTypes.element.isRequired,
+    /**
+     * Injected by the documentation to work in an iframe.
+     * You won't need it on your project.
+     */
+    window: PropTypes.func,
+  };
+
 export default function Header(props) {
     return (
         <AppBar position="fixed">

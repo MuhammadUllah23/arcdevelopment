fc4838b8e396e076f4d482114372820bb46526b8
Muhammad <muhammadhullah23@gmail.com>
2022-01-28 17:55:13 -0500

    create palette object

--- src/components/ui/Theme.js
+++ src/components/ui/Theme.js
@@ -1,5 +1,10 @@
 import { createMuiTheme } from "@material-ui/core"
 
 export default createMuiTheme({
-
+    palette: {
+        common: {
+            arcBlue: "#0B72B9",
+            arcOrange: "#FFBA60"
+        }
+    }
 })

88bd8f9d916f951b65eb980190593c5c140c2408
Muhammad <muhammadhullah23@gmail.com>
2022-01-28 16:13:22 -0500

    Appbar position 'fixed'
    Postion = 'fixed'. 'fixed' is the default position for Appbar but its their just remind the what the position is

--- src/components/ui/header.js
+++ src/components/ui/header.js
@@ -4,8 +4,9 @@ import { Toolbar } from "@material-ui/core/";
 
 export default function Header(props) {
     return (
-        <AppBar>
+        <AppBar position="fixed">
             <Toolbar>Arc Development</Toolbar>
+            {/* Toolbar helps lay content out in a horizontal manner */}
         </AppBar>
     )
 }

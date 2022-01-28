d27c642f39c5404b4c585ef3df3d4e25c5e733eb
Muhammad <muhammadhullah23@gmail.com>
2022-01-28 16:08:38 -0500

    Header Appbar and Toolbar
    Open up with Appber tag and Toolbar tag inside of Appbar. import Headers file in App.js Place the header on top of hello in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import Header from './ui/header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       Hello
     </div>
   );
--- src/components/ui/header.js
+++ src/components/ui/header.js
@@ -1,9 +1,11 @@
 import React from "react";
-import { AppBar } from "@material-ui/core/AppBar";
-import { Toolbar } from "@material-ui/core/Toolbar";
+import { AppBar } from "@material-ui/core/";
+import { Toolbar } from "@material-ui/core/";
 
 export default function Header(props) {
     return (
-        
+        <AppBar>
+            <Toolbar>Arc Development</Toolbar>
+        </AppBar>
     )
 }

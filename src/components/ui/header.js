a1d7aa99ac22c78a9474e464f7dc16f719f6a7e7
Muhammad <muhammadhullah23@gmail.com>
2022-01-28 18:19:21 -0500

    import makeStyles function
    It iss jss. Basically like css but in javascript

--- src/components/ui/header.js
+++ src/components/ui/header.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { AppBar } from "@material-ui/core/";
 import { Toolbar } from "@material-ui/core/";
+import Typography from "@material-ui/core/Typography";
 import useScrollTrigger from "@material-ui/core/useScrollTrigger";
+import { makeStyles } from "@material-ui/styles";
 
 function ElevationScroll(props) {
     const { children } = props;
@@ -17,13 +19,19 @@ function ElevationScroll(props) {
       elevation: trigger ? 4 : 0,
     });
   }
+
+  const usedStyle = makeStyles()
   
  
 export default function Header(props) {
     return (
         <ElevationScroll >
-        <AppBar position="fixed" color="primary">
-            <Toolbar>Arc Development</Toolbar>
+        <AppBar position="fixed" >
+            <Toolbar>
+                <Typography variant="h3">
+                    Arc Development
+                </Typography>
+            </Toolbar>
             {/* Toolbar helps lay content out in a horizontal manner */}
         </AppBar>
         </ElevationScroll>

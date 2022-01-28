747595dadbb974e4f4f31561d4521d9c58a51dc6
Muhammad <muhammadhullah23@gmail.com>
2022-01-28 16:48:38 -0500

    npm install @mui/styles

--- src/components/App.js
+++ src/components/App.js
@@ -5,14 +5,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      {[...new Array(120)]
-            .map(
-              () => `Cras mattis consectetur purus sit amet fermentum.
-Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
-            )
-            .join('\n')}
+      Hello
     </div>
   );
 }

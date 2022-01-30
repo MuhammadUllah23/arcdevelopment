b031da5bbfc80d7669788ba5445cfce347bda5d1
Muhammad <muhammadhullah23@gmail.com>
2022-01-29 23:25:54 -0500

    Active Tab Refresh Fix

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -63,8 +63,20 @@ export default function Header(props) {
     };
 
     useEffect(() => {
-        
-    })
+        if (window.location.pathname === "/" && value !== 0) {
+            setValue(0)
+        } else if (window.location.pathname === "/services" && value !== 1) {
+            setValue(1)
+        } else if (window.location.pathname === "/revolution" && value !== 2) {
+            setValue(2)
+        } else if (window.location.pathname === "/about" && value !== 3) {
+            setValue(3)
+        } else if (window.location.pathname === "/contact" && value !== 4) {
+            setValue(4)
+        } 
+    }, [value] ) 
+    // Value constant is being passed as dependancy which tells the usEffect hook that we are depending on the state value 
+    // and we dont want to run the code again if the value has not changed.
     return (
         <React.Fragment>
         <ElevationScroll >

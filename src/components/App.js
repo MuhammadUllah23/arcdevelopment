ac6eee2b4cb413bfd22fbf066a3d9eed7673bd2f
Muhammad <muhammadhullah23@gmail.com>
2022-01-29 22:53:41 -0500

    Set up 8 more routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import ThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 import theme from './ui/Theme'
@@ -10,6 +10,17 @@ function App() {
     <ThemeProvider theme={theme}> 
       <BrowserRouter>
         <Header />
+        <Routes>
+          <Route path="/" element={() => <div>Home</div>}/>
+          <Route path="/services" element={() => <div>services</div>}/>
+          <Route path="/customsoftware" element={() => <div>customsoftware</div>}/>
+          <Route path="/mobileapps" element={() => <div>mobileapps</div>}/>
+          <Route path="/websites" element={() => <div>websites</div>}/>
+          <Route path="/revolution" element={() => <div>revolution</div>}/>
+          <Route path="/about" element={() => <div>about</div>}/>
+          <Route path="/contact" element={() => <div>contact</div>}/>
+          <Route path="/estimate" element={() => <div>estimate</div>}/>
+        </Routes>
       </BrowserRouter>
     </ThemeProvider>
       

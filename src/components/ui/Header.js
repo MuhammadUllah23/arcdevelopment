fbdb26e9ff9a7c7520d971df9cd56d5848bdf9ba
Muhammad <muhammadhullah23@gmail.com>
2022-01-30 20:24:35 -0500

    Refactor useEffect
    Refactor using if and else if statement to switch cases

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -110,17 +110,70 @@ export default function Header(props) {
     ]
 
     useEffect(() => {
-        if (window.location.pathname === "/" && value !== 0) {
-            setValue(0)
-        } else if (window.location.pathname === "/services" && value !== 1) {
-            setValue(1)
-        } else if (window.location.pathname === "/revolution" && value !== 2) {
-            setValue(2)
-        } else if (window.location.pathname === "/about" && value !== 3) {
-            setValue(3)
-        } else if (window.location.pathname === "/contact" && value !== 4) {
-            setValue(4)
-        } 
+        // if (window.location.pathname === "/" && value !== 0) {
+        //     setValue(0)
+        // } else if (window.location.pathname === "/services" && value !== 1) {
+        //     setValue(1)
+        // } else if (window.location.pathname === "/revolution" && value !== 2) {
+        //     setValue(2)
+        // } else if (window.location.pathname === "/about" && value !== 3) {
+        //     setValue(3)
+        // } else if (window.location.pathname === "/contact" && value !== 4) {
+        //     setValue(4)
+        // } 
+
+        switch (window.location.pathname) {
+            case "/":
+                if (value !== 0) {
+                    setValue(0)
+                }
+                break;
+            case "/services":
+                if (value !== 1) {
+                    setValue(1)
+                    setSelectedIndex(0)
+                }
+                break;
+            case "/customsoftware":
+                if (value !== 1) {
+                    setValue(1)
+                    setSelectedIndex(1)
+                }
+                break;   
+            case "/mobileapps":
+                if (value !== 1) {
+                    setValue(1)
+                    setSelectedIndex(2)
+                }
+                break;
+            case "/websites":
+                if (value !== 1) {
+                    setValue(1)
+                    setSelectedIndex(3)
+                }
+                break;
+            case "/revolution":
+                if (value !== 2) {
+                    setValue(2)
+                }
+                break;   
+            case "/about":
+                if (value !== 3) {
+                    setValue(3)
+                }
+                break;
+            case "/contact":
+                if (value !== 4) {
+                    setValue(4)
+                }
+                break;
+            case "/estimate":
+                if (value !== 5) {
+                    setValue(5)
+                }     
+                default:
+                    break;
+        }
     }, [value] ) 
     // Value constant is being passed as dependancy which tells the usEffect hook that we are depending on the state value 
     // and we dont want to run the code again if the value has not changed.
@@ -168,7 +221,7 @@ export default function Header(props) {
                         to={option.link} 
                         classes={{root: classes.menuItem}} 
                         onClick={(event) => {handleMenuItemClick(event, i); setValue(1); handleClose()}} 
-                        selected={i === selectedIndex}> 
+                        selected={i === selectedIndex && value === 1}> 
                             {option.name} 
                         </MenuItem>
                     ))}

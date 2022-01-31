7ba394a39823b9d5c96b3b820df02a709ae0f9af
Muhammad <muhammadhullah23@gmail.com>
2022-01-31 12:48:58 -0500

    move content into tabs variable
    Put everything between toolbar in the const tabs in between the react fragment

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -113,17 +113,6 @@ export default function Header(props) {
     ]
 
     useEffect(() => {
-        // if (window.location.pathname === "/" && value !== 0) {
-        //     setValue(0)
-        // } else if (window.location.pathname === "/services" && value !== 1) {
-        //     setValue(1)
-        // } else if (window.location.pathname === "/revolution" && value !== 2) {
-        //     setValue(2)
-        // } else if (window.location.pathname === "/about" && value !== 3) {
-        //     setValue(3)
-        // } else if (window.location.pathname === "/contact" && value !== 4) {
-        //     setValue(4)
-        // } 
 
         switch (window.location.pathname) {
             case "/":
@@ -180,12 +169,10 @@ export default function Header(props) {
     }, [value] ) 
     // Value constant is being passed as dependancy which tells the usEffect hook that we are depending on the state value 
     // and we dont want to run the code again if the value has not changed.
-    return (
+
+    const tabs = (
         <React.Fragment>
-        <ElevationScroll >
-        <AppBar position="fixed" >
-            <Toolbar disableGutters>
-                <Button component={Link} to="/" disableRipple className={classes.logoContainer} onClick={() => setValue(0)}>
+            <Button component={Link} to="/" disableRipple className={classes.logoContainer} onClick={() => setValue(0)}>
                     <img alt="company logo" className={classes.logo} src={logo} />
                 </Button>
                 <Tabs 
@@ -229,6 +216,14 @@ export default function Header(props) {
                         </MenuItem>
                     ))}
                 </Menu>
+        </React.Fragment>
+    )
+    return (
+        <React.Fragment>
+        <ElevationScroll >
+        <AppBar position="fixed" >
+            <Toolbar disableGutters>
+                
             </Toolbar>
             {/* Toolbar helps lay content out in a horizontal manner */}
         </AppBar>

6c0a900ff78f07cb350e51bcb27f7d7df56621c5
Muhammad <muhammadhullah23@gmail.com>
2022-02-01 11:39:18 -0500

    toolbar margin responsive breakpoints

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -35,7 +35,13 @@ function ElevationScroll(props) {
   const useStyles = makeStyles(theme => ({
       toolbarmargin: {
           ...theme.mixins.toolbar,
-          marginBottom: "3em"
+          marginBottom: "3em",
+          [theme.breakpoints.down('md')]: {
+              marginBottom: '2em'
+          },
+          [theme.breakpoints.down('xs')]: {
+              marginBottom: '1.25em'
+          }
       },
       logo: {
           height: "8em",

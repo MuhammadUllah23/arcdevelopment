2aad3aa1308fee298d7ba65405b921d0a2ef469f
Muhammad <muhammadhullah23@gmail.com>
2022-02-05 16:51:21 -0500

    Creat const routes.
    Array of objects with name and link for each route

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -159,10 +159,17 @@ export default function Header(props) {
         {name: "Custom Software Development", link: "/customsoftware"}, 
         {name: "Mobile Apps Development", link: "/mobileapps"}, 
         {name: "Website Development", link: "/websites"}
+    ];
+
+    const routes = [
+        {name: "Home", link: "/"},
+        {name: "Services", link: "/services"},
+        {name: "The Revolution", link: "/revolution"},
+        {name: "About Us", link: "/about"},
+        {name: "Contact Us", link: "/contact"}
     ]
 
     useEffect(() => {
-
         switch (window.location.pathname) {
             case "/":
                 if (value !== 0) {

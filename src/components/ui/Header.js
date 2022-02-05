4bcd12795b5060a77f47168d67d0d24ab0f89dfc
Muhammad <muhammadhullah23@gmail.com>
2022-02-05 17:00:43 -0500

    Clean up and refactor useEffect
    spread menuOption and Routes in an array and use forEach to iterate through to pass into the switch case statement

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -170,59 +170,22 @@ export default function Header(props) {
     ]
 
     useEffect(() => {
-        switch (window.location.pathname) {
-            case "/":
-                if (value !== 0) {
-                    setValue(0)
-                }
-                break;
-            case "/services":
-                if (value !== 1) {
-                    setValue(1)
-                    setSelectedIndex(0)
-                }
-                break;
-            case "/customsoftware":
-                if (value !== 1) {
-                    setValue(1)
-                    setSelectedIndex(1)
-                }
-                break;   
-            case "/mobileapps":
-                if (value !== 1) {
-                    setValue(1)
-                    setSelectedIndex(2)
-                }
-                break;
-            case "/websites":
-                if (value !== 1) {
-                    setValue(1)
-                    setSelectedIndex(3)
-                }
-                break;
-            case "/revolution":
-                if (value !== 2) {
-                    setValue(2)
-                }
-                break;   
-            case "/about":
-                if (value !== 3) {
-                    setValue(3)
-                }
-                break;
-            case "/contact":
-                if (value !== 4) {
-                    setValue(4)
-                }
-                break;
-            case "/estimate":
-                if (value !== 5) {
-                    setValue(5)
-                }     
-                default:
+        [...menuOptions, ...routes].forEach(route => {
+            switch (window.location.pathname) {
+                case `${route.link}`:
+                    if (value !== route.activeIndex) {
+                        setValue(route.activeIndex)
+                        if (route.selectedIndex && route.selectedIndex !== selectedIndex) {
+                            setSelectedIndex(route.selectedIndex)
+                        }
+                    }
                     break;
-        }
-    }, [value] ) 
+                    default:
+                        break;
+            }
+        })
+       
+    }, [value, menuOptions, selectedIndex, routes] ) 
     // Value constant is being passed as dependancy which tells the usEffect hook that we are depending on the state value 
     // and we dont want to run the code again if the value has not changed.
 

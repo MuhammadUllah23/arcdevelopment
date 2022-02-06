78ba767ec2d00b9b24ddee8bb717806d72fbea72
Muhammad <muhammadhullah23@gmail.com>
2022-02-05 21:15:23 -0500

    Create basic footer style
    useStyle() set to const classes in Footer function

--- src/components/ui/Footer.js
+++ src/components/ui/Footer.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import {makeStyles} from '@material-ui/core/styles';
 
-const useStyles = makeStyles(theme => {
-    
-})
+const useStyles = makeStyles(theme => ({
+    footer: {
+        backgroundColor: theme.palette.common.blue,
+        width: "100%"
+    }
+}))
 
 export default function Footer() {
+    const classes = useStyles()
 
 }

a627e0fff59ba16b9ae45806774997a6dc478ff4
Muhammad <muhammadhullah23@gmail.com>
2022-02-05 21:26:59 -0500

    Route compoenent fix for element to return some content

--- src/components/App.js
+++ src/components/App.js
@@ -13,15 +13,15 @@ function App() {
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route path="/" element={() => <div>Home</div>}/>
-          <Route path="/services" element={() => <div>services</div>}/>
-          <Route path="/customsoftware" element={() => <div>customsoftware</div>}/>
-          <Route path="/mobileapps" element={() => <div>mobileapps</div>}/>
-          <Route path="/websites" element={() => <div>websites</div>}/>
-          <Route path="/revolution" element={() => <div>revolution</div>}/>
-          <Route path="/about" element={() => <div>about</div>}/>
-          <Route path="/contact" element={() => <div>contact</div>}/>
-          <Route path="/estimate" element={() => <div>estimate </div>}/>
+          <Route path="/" element={<div>Home</div>}/>
+          <Route path="/services" element={<div>services</div>}/>
+          <Route path="/customsoftware" element={<div>customsoftware</div>}/>
+          <Route path="/mobileapps" element={<div>mobileapps</div>}/>
+          <Route path="/websites" element={<div>websites</div>}/>
+          <Route path="/revolution" element={<div>revolution</div>}/>
+          <Route path="/about" element={<div>about</div>}/>
+          <Route path="/contact" element={<div>contact</div>}/>
+          <Route path="/estimate" element={<div>estimate </div>}/>
         </Routes>
         <Footer />
       </BrowserRouter>

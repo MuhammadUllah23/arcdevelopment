cd9db13cd5a0e0399f033ad9c1144d10eb948688
Muhammad <muhammadhullah23@gmail.com>
2022-02-06 19:06:59 -0500

    install react lottie

--- src/components/App.js
+++ src/components/App.js
@@ -17,15 +17,15 @@ function App() {
       <BrowserRouter>
         <Header value={value} setValue={setValue} selectedIndex={selectedIndex} setSelectedIndex={setSelectedIndex} />
         <Routes>
-          <Route path="/" element={<div style={{height: "300px"}}>Home</div>}/>
-          <Route path="/services" element={<div style={{height: "300px"}}>services</div>}/>
-          <Route path="/customsoftware" element={<div style={{height: "300px"}}>customsoftware</div>}/>
-          <Route path="/mobileapps" element={<div style={{height: "300px"}}>mobileapps</div>}/>
-          <Route path="/websites" element={<div style={{height: "300px"}}>websites</div>}/>
-          <Route path="/revolution" element={<div style={{height: "300px"}}>revolution</div>}/>
-          <Route path="/about" element={<div style={{height: "300px"}}>about</div>}/>
-          <Route path="/contact" element={<div style={{height: "300px"}}>contact</div>}/>
-          <Route path="/estimate" element={<div style={{height: "300px"}}>estimate </div>}/>
+          <Route path="/" element={<div style={{height: "400px"}}>Home</div>}/>
+          <Route path="/services" element={<div style={{height: "400px"}}>services</div>}/>
+          <Route path="/customsoftware" element={<div style={{height: "400px"}}>customsoftware</div>}/>
+          <Route path="/mobileapps" element={<div style={{height: "400px"}}>mobileapps</div>}/>
+          <Route path="/websites" element={<div style={{height: "400px"}}>websites</div>}/>
+          <Route path="/revolution" element={<div style={{height: "400px"}}>revolution</div>}/>
+          <Route path="/about" element={<div style={{height: "400px"}}>about</div>}/>
+          <Route path="/contact" element={<div style={{height: "400px"}}>contact</div>}/>
+          <Route path="/estimate" element={<div style={{height: "400px"}}>estimate </div>}/>
         </Routes>
         <Footer value={value} setValue={setValue} selectedIndex={selectedIndex} setSelectedIndex={setSelectedIndex}/>
       </BrowserRouter>

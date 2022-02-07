cddbd3dc11efe7d701b704ee7b4f65ecd6103702
Muhammad <muhammadhullah23@gmail.com>
2022-02-06 19:11:09 -0500

    create new LandingPage file
    Import necessary files, lottie, animation Data

---
+++ src/components/LandingPage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Lottie from 'react-lottie';
+import {makeStyles} from '@material-ui/core/styles';
+
+import animationData from '../animations/landinganimation/data'
+
+const useStyles = makeStyles(theme => ({
+
+}))
+
+export default function LandingPage() {
+    const classes = useStyles()
+    
+    return (
+        <div>
+            Hello
+        </div>
+    )
+}

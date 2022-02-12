f97c2d616804212b6e9cfdda2296195c23293014
Muhammad <muhammadhullah23@gmail.com>
2022-02-12 16:49:55 -0500

    Copy Grid item with Learn More Button and Paste it in CallToAction.js

--- src/components/ui/CallToAction.js
+++ src/components/ui/CallToAction.js
@@ -21,6 +21,12 @@ export default function CallToAction() {
                     <Grid item>
                         <Typography variant='h2' >Simple Software.<br />Revolutionary</Typography>
                         <Typography variant='subtitle2' >The advantage of the 21st Century.</Typography>
+                        <Grid container item>
+                            <Button variant="outlined" style={{color: 'white', borderColor: 'white'}} className={classes.buttonLearnMoreServices} >
+                                <span style={{marginRight: 10}}> Learn More </span>
+                                <ButtonArrow width={10} height={10} fill='white' />
+                            </Button>
+                        </Grid>
                     </Grid>
                 </Grid>
             </Grid>

ea4566d58cb453f79e48690220b0f43567fa2a88
Muhammad <muhammadhullah23@gmail.com>
2022-02-12 16:38:51 -0500

    New file CallToAction.js
    import files and create useStyles

---
+++ src/components/ui/CallToAction.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Grid } from "@material-ui/core";
+import { Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles(theme => ({
+
+}))
+
+export default function CallToAction() {
+    const classes = useStyles()
+
+    return (
+
+    )
+}

2f31712315108ba06aa1a431e9679630367d7474
Muhammad <muhammadhullah23@gmail.com>
2022-02-13 14:18:48 -0500

    New Service file page

---
+++ src/components/Services.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import { makeStyles } from "@material-ui/core";
+import { Grid } from "@material-ui/core";
+import { Typography } from "@material-ui/core";
+import { Button } from "@material-ui/core";
+
+const useStyles = makeStyles(theme => ({
+
+}))
+
+export default function Services() {
+    const classes = useStyles()
+
+    return (
+        
+    )
+}

ae87b33a029c294d844da846a696c61a520b2c74
Muhammad <muhammadhullah23@gmail.com>
2022-02-14 10:49:03 -0500

    Grid Fix

--- src/components/CustomSoftware.js
+++ src/components/CustomSoftware.js
@@ -112,29 +112,31 @@ export default function CustomSoftware(props) {
                     </Grid>
                 </Grid>
             </Grid>
-            <Grid item container direction='row' className={classes.itemContainer} >
-                <Grid item container >
-                    <Grid item container direction='column' >
-                        <Typography variant='h4'>Digital Documents & Data</Typography>
+            <Grid item container direction='row'  >
+                <Grid item container className={classes.itemContainer}>
+                    <Grid item container direction='column' md>
+                        <Grid item>
+                            <Typography variant='h4'>Digital Documents & Data</Typography>
+                        </Grid>
+                        <Grid item>
+                            <Typography variant='body1' paragraph>
+                                Reduce Errors. Reduce Waste. Reduce Costs.
+                            </Typography>
+                            <Typography variant='body1' paragraph>
+                                Billions are spent annually on the purchasing, printing, and
+                                distribution of paper. On top of the massive environmental
+                                impact this has, it causes harm to your bottom line as well.
+                            </Typography>
+                            <Typography variant='body1' paragraph>
+                                By utilizing digital forms and documents you can remove these
+                                obsolete expenses, accelerate your communication, and help the
+                                Earth.
+                            </Typography>
+                        </Grid>
+                    </Grid>    
+                    <Grid item md>
+                        <Lottie options={documentsOptions} style={{maxHeight: 325, maxWidth: 275, minHeight: 275}} />
                     </Grid>
-                    <Grid item>
-                        <Typography variant='body1' paragraph>
-                            Reduce Errors. Reduce Waste. Reduce Costs.
-                        </Typography>
-                        <Typography variant='body1' paragraph>
-                            Billions are spent annually on the purchasing, printing, and
-                            distribution of paper. On top of the massive environmental
-                            impact this has, it causes harm to your bottom line as well.
-                        </Typography>
-                        <Typography variant='body1' paragraph>
-                            By utilizing digital forms and documents you can remove these
-                            obsolete expenses, accelerate your communication, and help the
-                            Earth.
-                        </Typography>
-                    </Grid>
-                </Grid>
-                <Grid item>
-                    <Lottie options={documentsOptions} />
                 </Grid>
             </Grid>
         </Grid>

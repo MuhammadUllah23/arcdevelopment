b83a5ee8f1c9b0c84230b2ca8308e48a3e20b980
Muhammad <muhammadhullah23@gmail.com>
2022-02-14 11:02:08 -0500

    align text to right

--- src/components/CustomSoftware.js
+++ src/components/CustomSoftware.js
@@ -151,13 +151,12 @@ export default function CustomSoftware(props) {
                     <Grid item md>
                         <Lottie options={scaleOptions} style={{maxHeight: 260, maxWidth: 280}} />
                     </Grid>
-                    <Grid item container direction='column' md>
-                        
+                    <Grid item container direction='column' md>             
                         <Grid item>
-                            <Typography variant='h4'>Scale</Typography>
+                            <Typography variant='h4' align='right'>Scale</Typography>
                         </Grid>
                         <Grid item>
-                            <Typography variant='body1' paragraph>
+                            <Typography variant='body1' align='right' paragraph>
                                 Whether you're a large brand, just getting started, or taking
                                 off right now, our application architecture ensures pain-free
                                 growth and reliability.

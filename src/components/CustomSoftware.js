f91d3fda26d091ababf25ecd163b499381cfe8b7
Muhammad <muhammadhullah23@gmail.com>
2022-02-15 02:34:37 -0500

    Copy and paste Grid item container row

--- src/components/CustomSoftware.js
+++ src/components/CustomSoftware.js
@@ -184,6 +184,50 @@ export default function CustomSoftware(props) {
                     </Grid>
                 </Grid>
             </Grid>
+            <Grid item container direction='row' justify='space-between' >
+                <Grid item container className={classes.itemContainer} md>
+                    <Grid item container direction='column' md>
+                        <Grid item>
+                            <Typography variant='h4'>Digital Documents & Data</Typography>
+                        </Grid>
+                        <Grid item>
+                            <Typography variant='body1' paragraph>
+                                Reduce Errors. Reduce Waste. Reduce Costs.
+                            </Typography>
+                            <Typography variant='body1' paragraph>
+                                Billions are spent annually on the purchasing, printing, and
+                                distribution of paper. On top of the massive environmental
+                                impact this has, it causes harm to your bottom line as well.
+                            </Typography>
+                            <Typography variant='body1' paragraph>
+                                By utilizing digital forms and documents you can remove these
+                                obsolete expenses, accelerate your communication, and help the
+                                Earth.
+                            </Typography>
+                        </Grid>
+                    </Grid>    
+                    <Grid item md>
+                        <Lottie options={documentsOptions} style={{maxHeight: 275, maxWidth: 275, minHeight: 250}} />
+                    </Grid>
+                </Grid>
+                <Grid item container className={classes.itemContainer} md>
+                    <Grid item md>
+                        <Lottie options={scaleOptions} style={{maxHeight: 260, maxWidth: 280}} />
+                    </Grid>
+                    <Grid item container direction='column' md>             
+                        <Grid item>
+                            <Typography variant='h4' align='right'>Scale</Typography>
+                        </Grid>
+                        <Grid item>
+                            <Typography variant='body1' align='right' paragraph>
+                                Whether you're a large brand, just getting started, or taking
+                                off right now, our application architecture ensures pain-free
+                                growth and reliability.
+                            </Typography>
+                        </Grid>
+                    </Grid>                      
+                </Grid>
+            </Grid>
         </Grid>
     )
 }

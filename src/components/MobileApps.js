473aec127c16fbb2c6d34ff8abd162e00cfe96a9
Muhammad <muhammadhullah23@gmail.com>
2022-02-18 18:57:56 -0500

    Create and start Mobile Apps page

---
+++ src/components/MobileApps.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Lottie from "react-lottie";
+import { Link } from "react-router-dom";
+
+import { makeStyles, useTheme } from "@material-ui/core";
+import { Grid } from "@material-ui/core";
+import { Typography } from "@material-ui/core";
+import { useMediaQuery } from "@material-ui/core";
+import { Hidden } from "@material-ui/core";
+
+const useStyles = makeStyles(theme => ({
+
+}))
+
+export default function MobileApps() {
+    const classes = useStyles()
+    const them = useTheme()
+
+    return (
+        
+    )
+}

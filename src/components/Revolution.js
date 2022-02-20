9a561ab4bd842987f2f99ce85398e44a1e706831
Muhammad <muhammadhullah23@gmail.com>
2022-02-19 22:19:09 -0500

    create and start Revolution page

---
+++ src/components/Revolution.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+import {makeStyles, useTheme} from '@material-ui/core/styles';
+import { Grid } from '@material-ui/core';
+import { Typography } from '@material-ui/core';
+import { useMediaQuery } from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+
+}))
+
+export default function Revolution() {
+    const classes = useStyles()
+    const theme = useTheme()
+
+    return (
+        <Grid container direction="column" >
+            
+        </Grid>
+    )
+}

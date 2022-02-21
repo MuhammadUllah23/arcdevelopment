7b4c84aea0aca95db3a014bf8c6142a29109550c
Muhammad <muhammadhullah23@gmail.com>
2022-02-20 22:30:53 -0500

    usual const variables

--- src/components/About.js
+++ src/components/About.js
@@ -11,6 +11,11 @@ const useStyles = makeStyles(theme => ({
 }))
 
 export default function About(props) {
+    const classes = useStyles()
+    const theme = useTheme()
+    const matchesMD = useMediaQuery(theme.breakpoints.down('md'))
+    const matchesSM = useMediaQuery(theme.breakpoints.down('sm'))
+    const matchesXS = useMediaQuery(theme.breakpoints.down('xs'))
 
     return (
         <Grid container direction="column" >

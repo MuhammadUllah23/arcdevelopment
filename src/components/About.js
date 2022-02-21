08f321301ea9797a153d4c3131a35ed857ffc296
Muhammad <muhammadhullah23@gmail.com>
2022-02-20 22:38:31 -0500

    Style mission statement

--- src/components/About.js
+++ src/components/About.js
@@ -5,9 +5,16 @@ import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import useMediaQuery from "@material-ui/core/useMediaQuery";
 import Hidden from "@material-ui/core/Hidden";
+import { LineWeight } from "@material-ui/icons";
 
 const useStyles = makeStyles(theme => ({
-
+    missionStatement: {
+        fontStyle: "italic",
+        fontWeight: 300,
+        fontSize: "1.5rem",
+        maxWidth: "50em",
+        lineHeight: 1.4
+    }
 }))
 
 export default function About(props) {
@@ -23,7 +30,7 @@ export default function About(props) {
                 <Typography variant="h2">About US</Typography>
             </Grid>
             <Grid item>
-                <Typography variant="h2">
+                <Typography variant="h2" className={classes.missionStatement}>
                     Whether it be person to person, business to consumer, or an individual
                     to their interests, technology is meant to bring us closer to what we
                     care about in the best way possible. Arc Development will use that

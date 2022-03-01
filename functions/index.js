8d0b0f0323680aa52144503fcc53db82228b495d
Muhammad <muhammadhullah23@gmail.com>
2022-03-01 13:40:29 -0500

    start transporter

--- functions/index.js
+++ functions/index.js
@@ -5,7 +5,9 @@ const config = functions.config();
 
 admin.initializeApp();
 
-
+const transporter = nodemailer.createTransport({
+    
+})
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //

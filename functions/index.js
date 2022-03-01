36473ab6ccb095ed2f9057c534074ce7c0f19027
Muhammad <muhammadhullah23@gmail.com>
2022-03-01 11:41:32 -0500

    set up functions to set up firebase

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,8 @@
 const functions = require("firebase-functions");
-
+const config = functions.config();
+const admin = require("firebase-admin");
+const { initializeApp } = require("firebase-admin");
+admin initializeApp()
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //

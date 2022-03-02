4156eb4c5efa3b24816f60bec35c9e470724b5c6
Muhammad <muhammadhullah23@gmail.com>
2022-03-02 02:06:06 -0500

    set email and password

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,11 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
-const nodemailer = require("nodemailer") 
+const nodemailer = require("nodemailer"); 
 const config = functions.config();
 
 admin.initializeApp();
 
-const transporter = nodemailer.createTransport({
-    
-})
+const transporter = nodemailer.createTransport({service: "Gmail", auth: {}});
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //

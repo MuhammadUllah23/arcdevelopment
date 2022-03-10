b9569370b9e59fa037b6eda6e487f653790ba0ea
Muhammad <muhammadhullah23@gmail.com>
2022-03-10 18:35:12 -0500

    overide old firebase project with a new one

--- functions/index.js
+++ functions/index.js
@@ -1,33 +1,10 @@
 const functions = require("firebase-functions");
-const admin = require("firebase-admin");
-const nodemailer = require("nodemailer");
-const config = functions.config();
-const cors = require("cors")({origin: true});
+const 
 
-admin.initializeApp();
-
-const transporter = nodemailer.createTransport({
-  service: "Gmail",
-  auth: {user: config.user.email,
-    pass: config.user.password},
-});
-
-const mailOptions = {from: "Arc Development",
-  to: "muhammadhullah23@gmail.com",
-  subject: "Testing nodemailer",
-  text: "Test Successful"};
-
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-exports.sendMail = functions.https.onRequest((request, response) => {
-  cors(request, response, () => {
-    transporter.sendMail(mailOptions, (error) => {
-      if (error) {
-        response.send(error);
-      } else {
-        response.send("Message sent successfully");
-      }
-    });
-  });
-});
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });

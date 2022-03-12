0e677c06d68725bfb78b618cf3ff3e273c882945
Muhammad <muhammadhullah23@gmail.com>
2022-03-12 13:53:43 -0500

    import all images and animations needed for page

--- src/components/Estimate.js
+++ src/components/Estimate.js
@@ -9,6 +9,32 @@ import { Button } from "@material-ui/core";
 import { useMediaQuery } from "@material-ui/core"
 import { IconButton } from '@material-ui/core';
 
+import check from "../assets/check.svg";
+import send from "../assets/send.svg";
+import software from "../assets/software.svg";
+import mobile from "../assets/mobile.svg";
+import website from "../assets/website.svg";
+import backArrow from "../assets/backArrow.svg";
+import backArrowDisabled from "../assets/backArrowDisabled.svg";
+import forwardArrow from "../assets/forwardArrow.svg";
+import forwardArrowDisabled from "../assets/forwardArrowDisabled.svg";
+import camera from "../assets/camera.svg";
+import upload from "../assets/upload.svg";
+import person from "../assets/person.svg";
+import persons from "../assets/persons.svg";
+import info from "../assets/info.svg";
+import bell from "../assets/bell.svg";
+import people from "../assets/people.svg";
+import usersIcon from "../assets/users.svg";
+import iPhone from "../assets/iphone.svg";
+import gps from "../assets/gps.svg";
+import customized from "../assets/customized.svg";
+import data from "../assets/data.svg";
+import android from "../assets/android.svg";
+import globe from "../assets/globe.svg";
+import biometrics from "../assets/biometrics.svg";
+import estimateAnimation from "../animations/estimateAnimation/data.json"
+
 const useStyle = makeStyles((theme) => {
 
 })
@@ -19,7 +45,7 @@ export default function Estimate() {
 
     return (
         <Grid container direction="column" >
-            
+
         </Grid>       
     )
 }

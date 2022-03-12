8ec93b68b78c6f0077ca8eb3983d51ae2c76a2f8
Muhammad <muhammadhullah23@gmail.com>
2022-03-12 13:52:12 -0500

    create and start Estimate page

---
+++ src/components/Estimate.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Lottie from 'react-lottie'
+import { Link } from "react-router-dom";
+
+import { makeStyles, useTheme } from "@material-ui/styles";
+import { Grid } from "@material-ui/core";
+import { Typography } from "@material-ui/core";
+import { Button } from "@material-ui/core";
+import { useMediaQuery } from "@material-ui/core"
+import { IconButton } from '@material-ui/core';
+
+const useStyle = makeStyles((theme) => {
+
+})
+
+export default function Estimate() {
+    const theme = useTheme()
+    const classes = useStyle()
+
+    return (
+        <Grid container direction="column" >
+            
+        </Grid>       
+    )
+}

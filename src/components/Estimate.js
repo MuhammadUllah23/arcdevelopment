ca6f9c4834b3f88c6f53506425ebe5e145a71ea8
Muhammad <muhammadhullah23@gmail.com>
2022-03-25 19:17:05 -0400

    grid item for third options check

--- src/components/Estimate.js
+++ src/components/Estimate.js
@@ -617,6 +617,14 @@ export default function Estimate() {
                             <Typography variant="body1">Second options check</Typography>
                           </Grid>
                         </Grid>
+                        <Grid item container alignItems="center">
+                          <Grid item>
+                            <img src={check} alt="checkmark" />
+                          </Grid>
+                          <Grid item>
+                            <Typography variant="body1">Third options check</Typography>
+                          </Grid>
+                        </Grid>
                       </Grid>
                     </Grid>
                   </Grid>

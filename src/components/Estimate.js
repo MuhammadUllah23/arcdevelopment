17b021b9e6e0cc3b52bcd4c042e5579e69326fc1
Muhammad <muhammadhullah23@gmail.com>
2022-03-26 18:12:32 -0400

    copy and paste feature text template in second options check

--- src/components/Estimate.js
+++ src/components/Estimate.js
@@ -683,7 +683,37 @@ export default function Estimate() {
                             <img src={check} alt="checkmark" />
                           </Grid>
                           <Grid item>
-                            <Typography variant="body1">Second options check</Typography>
+                            <Typography variant="body1">
+                            {"with "}
+                        {/* if we have features... */}
+                        {features.length > 0
+                          ? //...and there's only 1...
+                            features.length === 1
+                              ? //then end the sentence here
+                              `${features[0]}.`
+                              : //otherwise, if there are two features...
+                            features.length === 2
+                              ? //...then end the sentence here
+                              `${features[0]} and ${features[1]}.`
+                              : //otherwise, if there are three or more features...
+                              features
+                            //filter out the very last feature...
+                            .filter(
+                              (feature, index) =>
+                              index !== features.length - 1
+                            )
+                            //and for those features return their name...
+                            .map((feature, index) => (
+                              <span key={index}>{`${feature}, `}</span>
+                            ))
+                          : null}
+                        {features.length > 0 &&
+                          features.length !== 1 &&
+                          features.length !== 2
+                            ? //...and then finally add the last feature with 'and' in front of it
+                            ` and ${features[features.length - 1]}.`
+                            : null}
+                            </Typography>
                           </Grid>
                         </Grid>
                         <Grid item container alignItems="center">
